f4bef0bc8ea286bd194bd7c3077eb82e7acee50b
KdjTheCoolestGuy123 <140424946+kdjthecoolguy123@users.noreply.github.com>
2025-04-22 11:13:10 +0200

    Update MainMenuStateNew.hx

--- source/states/MainMenuStateNew.hx
+++ source/states/MainMenuStateNew.hx
@@ -13,7 +13,6 @@ class MainMenuStateNew extends TitleState
   override function create()
 
 	var fnfVer:FlxText = new FlxText(12, FlxG.height - 24, 0, "Crazy Funker's v1.5 [Dev Build]", 12);
-	fnfVer.scrollFactor.set();
 	fnfVer.setFormat(Paths.font("vcr.ttf"), 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 	add(fnfVer);
 	changeItem();
