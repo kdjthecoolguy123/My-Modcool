556b6ab93910dd2baa80d2d8b70e1a361630c65b
kdjthecoolguy123 <140424946+kdjthecoolguy123@users.noreply.github.com>
2025-04-22 10:10:03 +0200

    Update and rename MainMenuState_new.hx to MainMenuStateNew.hx

---
+++ source/states/MainMenuStateNew.hx
@@ -0,0 +1,25 @@
+package;
+
+import.flixel.FlxG;
+import.flixel.FlxSprite;
+import.flixel.FlxState;
+import.flixel.text.FlxText;
+import.flixel.ui.FlxButton;
+import.flixel.ui.FlxMath;
+
+class MainMenuStateNew extends TitleState
+{
+
+  var sprite:FlxSprite;
+  override pubblic function create():void
+  {
+
+    super.create();
+    sprite = new.FlxSprite();
+    sprite.makeGraphic(300, 300, flixel.util.FlxColor.WHITE);
+    
+
+  }
+
+}
+
