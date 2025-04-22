32f5c60e6ee0aadcd3deb77d7408032d1c2c3645
kdjthecoolguy123 <140424946+kdjthecoolguy123@users.noreply.github.com>
2025-04-22 09:43:39 +0200

    Update MainMenuState.hx
    test1

--- source/states/MainMenuState.hx
+++ source/states/MainMenuState.hx
@@ -98,7 +98,7 @@ class MainMenuState extends MusicBeatState
 		psychVer.scrollFactor.set();
 		psychVer.setFormat(Paths.font("vcr.ttf"), 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(psychVer);
-		var fnfVer:FlxText = new FlxText(12, FlxG.height - 24, 0, "Friday Night Funkin' v" + Application.current.meta.get('version'), 12);
+		var fnfVer:FlxText = new FlxText(12, FlxG.height - 24, 0, "Crazy Funker's v1.5 [Dev Build]", 12);
 		fnfVer.scrollFactor.set();
 		fnfVer.setFormat(Paths.font("vcr.ttf"), 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(fnfVer);
