042225950ce3c256ca7471eca870d6dfbe9f16ec
KdjTheCoolestGuy123 <140424946+kdjthecoolguy123@users.noreply.github.com>
2025-04-25 10:45:33 +0200

    Update MainMenuStateNew.hx

--- source/states/MainMenuStateNew.hx
+++ source/states/MainMenuStateNew.hx
@@ -11,15 +11,13 @@ class MainMenuStateNew extends TitleState
 
 
 {
-
+	
 	var lol:FlxSprite;
 
   override function create()
 
     {
 	lol = new FlxSprite().loadGraphic(Paths.image(menuDesat));
-	lol.x = 200;
-	lol.y = 200;
 	add(lol);
 
 	var fnfVer:FlxText = new FlxText(12, FlxG.height - 400, 400, "[TEST]", 12);
