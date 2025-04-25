43a0acb7186dd98984c938959e53bfff88b1a07d
KdjTheCoolestGuy123 <140424946+kdjthecoolguy123@users.noreply.github.com>
2025-04-25 10:37:25 +0200

    Update MainMenuStateNew.hx

--- source/states/MainMenuStateNew.hx
+++ source/states/MainMenuStateNew.hx
@@ -1,6 +1,7 @@
 package states;
 
 import flixel.FlxObject;
+import flixel.FlxSprite;
 import flixel.effects.FlxFlicker;
 import lime.app.Application;
 import states.editors.MasterEditorMenu;
@@ -8,11 +9,17 @@ import options.OptionsState;
 
 class MainMenuStateNew extends TitleState
 
+	var lol:FlxSprite;
+
 {
 
   override function create()
 
     {
+	lol = new FlxSprite().loadGraphic(Paths.image(menuDesat));
+	lol.x = 200
+	lol.y = 200
+	add(lol);
 
 	var fnfVer:FlxText = new FlxText(12, FlxG.height - 400, 400, "[TEST]", 12);
 	add(fnfVer);
