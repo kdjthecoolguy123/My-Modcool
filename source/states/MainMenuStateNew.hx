30f000966f63f8242bce3265f38c6e75799cf598
KdjTheCoolGuy123 <kdjthecoolguy123@gmail.com>
2025-09-07 20:41:17 +0200

    freeplay addition and minor stuff

--- source/states/MainMenuStateNew.hx
+++ source/states/MainMenuStateNew.hx
@@ -15,6 +15,7 @@ class MainMenuStateNew extends TitleState
 
 	var background:FlxSprite;
 	var storyMode_button:FlxSprite;
+	var freeplay_button:FlxSprite;
 	var chapterSelect:Int = 1;
 	var allowMouse:Bool = true;
 
@@ -23,10 +24,18 @@ class MainMenuStateNew extends TitleState
 		background = new FlxSprite(0, FlxG.height * 0.52).loadGraphic(Paths.image('menuBG'));
 		background.screenCenter();
 		add(background);
+
+		//
+
 		storyMode_button = new FlxSprite(0, FlxG.height * 0.52).loadGraphic(Paths.image('go'));
+		storyMode_button.setPosition(0, 200)
 		add(storyMode_button);
 
-		var fnfVer:FlxText = new FlxText(12, FlxG.height - 400, 0, "Crazy Funker's" + updateVer, 12);
+		freeplay_button = new FlxSprite(0, FlxG.height * 0.52).loadGraphic(Paths.image('go'));
+		freeplay_button.setPosition(500, 200)
+		add(freeplay_button);
+
+		var fnfVer:FlxText = new FlxText(12, FlxG.height - 400, 0, "Crazy Funker's " + updateVer + " [Developer Build]", 12);
 		add(fnfVer);
 
     }
@@ -35,6 +44,10 @@ class MainMenuStateNew extends TitleState
 
 		if (FlxG.mouse.justPressed && storyMode_button.overlapsPoint(new FlxPoint(FlxG.mouse.screenX, FlxG.mouse.screenY))) {
     		MusicBeatState.switchState(new StoryMenuState());
+		}
+
+		if (FlxG.mouse.justPressed && freeplay_button.overlapsPoint(new FlxPoint(FlxG.mouse.screenX, FlxG.mouse.screenY))) {
+    		MusicBeatState.switchState(new FreeplayState());
 		}
 		
 	}
